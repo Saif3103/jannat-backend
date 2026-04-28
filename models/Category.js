80e056e36d7fafb5e5869cd5b9c3a291ac2e77b8
Saif Ali <saifali3031@gmail.com>
2026-04-29 03:15:18 +0530

    Fix: Permanently resolve 'next is not a function' in Category model using async middleware

--- models/Category.js
+++ models/Category.js
@@ -8,11 +8,10 @@ const categorySchema = new mongoose.Schema({
   isActive: { type: Boolean, default: true },
 }, { timestamps: true });
 
-categorySchema.pre('save', function (next) {
+categorySchema.pre('save', async function () {
   if (this.isModified('name')) {
     this.slug = this.name.toLowerCase().replace(/[^a-z0-9]+/g, '-');
   }
-  next();
 });
 
 module.exports = mongoose.model('Category', categorySchema);
